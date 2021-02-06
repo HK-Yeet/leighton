18a3eab191fe2f39c47fe6e289a8f39f50f5db3f
HK <68507957+HK-Yeet@users.noreply.github.com>
2021-02-06 13:34:43 -0700

    Update upload.js

--- src/commands/upload.js
+++ src/commands/upload.js
@@ -1,17 +1,22 @@
-const Discord = require('discord.js')
+const Discord = require("discord.js");
 
 module.exports = {
-    name: "upload",
-    cooldown: 60 * 60, //in seconds
-    clientPerms: ["SEND_MESSAGES"],
-    callback: async (bot, message, args, hkandler) => {
-        message.channel.send('<:YouTube:807695596235259986> Uploading your video...').then(message => {
-            setTimeout(() => {
-                message.edit({ embed: {
-                    title: 'Done!',
-                    description: 'Your video has been uploaded to YouTube!'
-                }})
-            }, 5000)
-        })
-    },
+  name: "upload",
+  cooldown: 60 * 60, //in seconds
+  clientPerms: ["SEND_MESSAGES"],
+  callback: async (bot, message, args, hkandler) => {
+    message.channel
+      .send("<:YouTube:807695596235259986> Uploading your video...")
+      .then((message) => {
+        setTimeout(() => {
+          message.edit("\u200B", {
+            embed: {
+              title: "Done!",
+              color: "RED",
+              description: "Your video has been uploaded to YouTube!",
+            },
+          });
+        }, 5000);
+      });
+  },
 };
