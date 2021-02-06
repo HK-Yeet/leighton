0da677df6a6e1eab644ca84c4058103b786f2573
Bqre <36790871+BqreXD@users.noreply.github.com>
2021-02-06 21:50:46 +0200

    Add files via upload

---
+++ src/commands/upload.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js')
+
+module.exports = {
+    name: "upload",
+    cooldown: 60 * 60, //in seconds
+    clientPerms: ["SEND_MESSAGES"],
+    callback: async (bot, message, args, hkandler) => {
+        message.channel.send('<:YouTube:807695596235259986> Uploading your video...').then(message => {
+            setTimeout(() => {
+                message.edit({ embed: {
+                    title: 'Done!',
+                    description: 'Your video has been uploaded to YouTube!'
+                }})
+            }, 5000)
+        })
+    },
+};
