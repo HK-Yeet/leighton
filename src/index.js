728c34e2d944c40a58bba302e645d7fa9113b8f8
hados827 <73777697+hados827@users.noreply.github.com>
2021-02-07 14:09:25 +0000

    Changed nothing

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
 const { HKandler } = require("hkutilities");
 const { Client } = require("discord.js");
-
 const { prefix, token, owners } = require("./config.json");
-
-const client = new Client();
+const client = new Client(); 
 
 let dirs = {
   commandsDir: "commands",
   eventsDir: "events",
   featuresDir: "features",
 };
+
 new HKandler(client, dirs)
   .setPrefix(prefix)
   .setOwners(owners)
