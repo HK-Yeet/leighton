7f628d0c33e86f3e3fc93424823edebe783b3faf
Bqre <36790871+BqreXD@users.noreply.github.com>
2021-02-21 21:48:16 +0200

    Update guildCreate.js

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -1,16 +1,17 @@
 const { MessageEmbed } = require("discord.js");
 module.exports = (bot, hkandler, guild) => {
-  const channel = guild.channels.cache.find(
-    (c) =>
-      c.type === "text" &&
-      c.permissionsFor(guild.me).has(["SEND_MESSAGES", "EMBED_LINKS"])
-  );
-
-  let embed = new MessageEmbed()
-    .setColor("BLUE")
-    .setTitle("👋 Hello There")
-    .setDescription(
-      `I'm ${bot.user.tag}! My prefix is \`${hkandler.prefix}\`.`
-    );
-  channel.send(embed);
+  console.log(`Leighton has been added to ${guild.name}.`)
+    let defaultChannel = "";
+    const generalChannel = guild.channels.cache.find(ch => ch.name === 'general' && ch.permissionsFor(guild.me).has('SEND_PERMISSIONS'))
+    if (generalChannel) defaultChannel = generalChannel
+    else {
+        guild.channels.cache.forEach((channel) => {
+            if (channel.type == "text" && defaultChannel === "") {
+                if (channel.permissionsFor(guild.me).has("SEND_MESSAGES")) {
+                    defaultChannel = channel;
+                }
+            }
+        })
+    }
+    defaultChannel.send('Hi there!\nThanks for inviting Leighton to your server.\n\nPlease type **\'!help\'** for a list of all of the bot\'s commands!\nIf you like our work, please make sure to vote for Leighton by typing **\'!upvote\'**. It\'ll help us a lot :)').catch(err => console.error(err))
 };
