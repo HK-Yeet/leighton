d8b86a71977960feb40165dbf4e891940276e0f3
HK <68507957+HK-Yeet@users.noreply.github.com>
2021-02-26 08:14:39 -0700

    Gitignore

--- node_modules/hkutilities/dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-const HKandler = require("./structures/hkandler");
-const utils_1 = require("./functions/utils");
-module.exports = { filter: utils_1.filter, quickEmbed: utils_1.quickEmbed, successEmbed: utils_1.successEmbed, errorEmbed: utils_1.errorEmbed, HKandler };
